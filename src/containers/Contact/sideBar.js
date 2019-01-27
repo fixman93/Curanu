e96de12e5dd1658ec9b6974fd91f937955801106
Boris Civcic <civcicboris93@gmail.com>
2019-01-27 14:00:03 +0100

    add: Contact form new files

---
+++ src/containers/Contact/sideBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import './SideBar.scss'
+class SideBar extends Component {
+  render() {
+    return (
+      <div className='sideBar'>
+        Sidebar
+      </div>
+    )
+  }
+}
+
+export default SideBar
